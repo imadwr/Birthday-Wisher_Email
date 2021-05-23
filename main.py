2e251d029357d7fc131ccbb618c8d784936eca88
imadwr <imadouairem96@gmail.com>
2021-05-23 20:23:30 +0100

    refactor message

--- main.py
+++ main.py
@@ -11,11 +11,7 @@ PASSWORD = os.getenv("PASSWORD")
 def send_birthday_email(persons_birthdays):
     letter_number = random.randint(1, 3)
     with open(f"./letter_templates/letter_{letter_number}.txt") as letter_file:
-        lines = letter_file.readlines()
-
-    message = ""
-    for line in lines:
-        message += line
+        message = letter_file.read()
 
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
